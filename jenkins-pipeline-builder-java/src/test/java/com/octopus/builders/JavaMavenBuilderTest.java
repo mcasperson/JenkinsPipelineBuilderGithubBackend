221660dacaf3e5329263bdb1070fcf97a73df7fc
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-04 14:00:20 +1000

    Cleaned up imports

--- jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
+++ jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
@@ -1,44 +1,23 @@
 package com.octopus.builders;
 
-import static org.jboss.logging.Logger.Level.DEBUG;
 import static org.junit.Assert.assertTrue;
 
 import com.google.common.io.Resources;
 import com.octopus.builders.java.JavaMavenBuilder;
 import com.octopus.jenkinsclient.JenkinsClient;
-import com.octopus.repoaccessors.RepoAccessor;
 import com.octopus.repoaccessors.TestRepoAccessor;
-import com.sun.source.tree.TryTree;
 import io.vavr.control.Try;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import lombok.NonNull;
 import org.apache.commons.text.StringEscapeUtils;
-import org.apache.http.Header;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpHead;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.MountableFile;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 @Testcontainers
 public class JavaMavenBuilderTest {
@@ -47,8 +26,8 @@ public class JavaMavenBuilderTest {
   private static final JenkinsClient JENKINS_CLIENT = new JenkinsClient();
 
   /**
-   * A Jenkins container that has the appropriate plugins installed, an admin user setup,
-   * the initial wizard disabled, and other customizations.
+   * A Jenkins container that has the appropriate plugins installed, an admin user setup, the
+   * initial wizard disabled, and other customizations.
    */
   @Container
   public GenericContainer jenkins = new GenericContainer(new ImageFromDockerfile()
@@ -73,7 +52,8 @@ public class JavaMavenBuilderTest {
       .withCopyFileToContainer(MountableFile.forClasspathResource("jenkins/java_tool.groovy"),
           "/usr/share/jenkins/ref/init.groovy.d/java_tool.groovy")
       .withExposedPorts(8080)
-      .withEnv("JAVA_OPTS", "-Djenkins.install.runSetupWizard=false -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true");
+      .withEnv("JAVA_OPTS",
+          "-Djenkins.install.runSetupWizard=false -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true");
 
   @ParameterizedTest
   @CsvSource({
@@ -82,8 +62,10 @@ public class JavaMavenBuilderTest {
       "https://github.com/mcasperson/SampleMavenProject-SpringBoot,maven,false",
       "https://github.com/mcasperson/SampleGradleProject-SpringBoot,gradle,false"
   })
-  public void verifyTemplate(final String url, final String name, final String useWrapper) throws IOException {
-    final String template = JAVA_MAVEN_BUILDER.generate(new TestRepoAccessor(url, useWrapper.equals("true")));
+  public void verifyTemplate(final String url, final String name, final String useWrapper)
+      throws IOException {
+    final String template = JAVA_MAVEN_BUILDER.generate(
+        new TestRepoAccessor(url, useWrapper.equals("true")));
 
     // Add the job to the docker image
     addJobToJenkins(getScriptJob(template), name);
@@ -93,16 +75,21 @@ public class JavaMavenBuilderTest {
 
     // Now restart jenkins, initiate a build, and check the build result
     final Try<Boolean> success =
-          // wait for the server to start
+        // wait for the server to start
         JENKINS_CLIENT.waitServerStarted(jenkins.getHost(), jenkins.getFirstMappedPort())
             // restart the server to pick up the new jobs
-            .flatMap(r -> JENKINS_CLIENT.restartJenkins(jenkins.getHost(), jenkins.getFirstMappedPort()))
+            .flatMap(
+                r -> JENKINS_CLIENT.restartJenkins(jenkins.getHost(), jenkins.getFirstMappedPort()))
             // wait for the server to start again
-            .flatMap(r -> JENKINS_CLIENT.waitServerStarted(jenkins.getHost(), jenkins.getFirstMappedPort()))
+            .flatMap(r -> JENKINS_CLIENT.waitServerStarted(jenkins.getHost(),
+                jenkins.getFirstMappedPort()))
             // start building the job
-            .flatMap(r -> JENKINS_CLIENT.startJob(jenkins.getHost(), jenkins.getFirstMappedPort(), name))
+            .flatMap(
+                r -> JENKINS_CLIENT.startJob(jenkins.getHost(), jenkins.getFirstMappedPort(), name))
             // wait for the job to finish
-            .flatMap(r -> JENKINS_CLIENT.waitJobBuilding(jenkins.getHost(), jenkins.getFirstMappedPort(), name))
+            .flatMap(
+                r -> JENKINS_CLIENT.waitJobBuilding(jenkins.getHost(), jenkins.getFirstMappedPort(),
+                    name))
             // see if the job was a success
             .map(JENKINS_CLIENT::isSuccess);
 
