f744c2fd5bff3e09008718c22bb2c41b04ac69f1
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-04 15:15:57 +1000

    Downloaded and extracted JDK 17

--- jenkins-pipeline-builder-java/src/test/resources/jenkins/java_tool.groovy
+++ jenkins-pipeline-builder-java/src/test/resources/jenkins/java_tool.groovy
@@ -5,4 +5,4 @@ import hudson.model.*;
 import hudson.tools.*
 
 dis = new hudson.model.JDK.DescriptorImpl();
-dis.setInstallations( new hudson.model.JDK("Java", "/opt/java/openjdk"));
+dis.setInstallations( new hudson.model.JDK("Java", "/opt/zulu17.28.13-ca-jdk17.0.0-linux_x64"));
