66a28b07d7c1b1ab187f41b01ad03d154899d907
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-06 13:51:24 +1000

    You actually cant call /git/trees with auth

--- jenkins-pipeline-builder-github-backend/src/main/java/com/octopus/repoclients/github/GithubRepoClient.java
+++ jenkins-pipeline-builder-github-backend/src/main/java/com/octopus/repoclients/github/GithubRepoClient.java
@@ -53,7 +53,9 @@ public class GithubRepoClient implements RepoClient {
     LOG.debug("GithubRepoClient.getFile(String)");
 
     return getDetails()
-        .flatMap(d -> getDefaultBranches().stream().map(b -> httpClient.get("https://raw.githubusercontent.com/" + d.getUsername() + "/" + d.getRepository() + "/" + b + "/" + path))
+        .flatMap(d -> getDefaultBranches().stream().map(b -> httpClient.get(
+                "https://raw.githubusercontent.com/" + d.getUsername() + "/" + d.getRepository() + "/"
+                    + b + "/" + path))
             .filter(Try::isSuccess)
             .findFirst()
             .orElse(Try.failure(new Exception("All attempts to find a file failed."))));
@@ -78,20 +80,24 @@ public class GithubRepoClient implements RepoClient {
 
     return getDetails()
         // Get the repository tree list
-        .flatMap(d -> httpClient.get(
-            "https://api.github.com/repos/" + d.getUsername() + "/" +  d.getRepository() + "/git/trees/master?recursive=1",
-            username,
-            password))
-        // Convert the resulting JSON into a map
-        .mapTry(j -> new ObjectMapper().readValue(j, Map.class))
-        // files are contained in the tree array
-        .mapTry(d -> (List<Map<Object, Object>>)d.get("tree"))
-        // each member of the tree array is an object containing a path
-        .mapTry(t -> t
+        .flatMap(d -> getDefaultBranches()
             .stream()
-            .map(u -> u.get("path").toString())
-            .filter(p -> ANT_PATH_MATCHER.matches(path, p))
-            .collect(Collectors.toList()));
+            .map(b -> httpClient.get(
+                "https://api.github.com/repos/" + d.getUsername() + "/" + d.getRepository()
+                    + "/git/trees/" + b + "?recursive=1"))
+            .filter(t -> t.isSuccess())
+            .findFirst()
+            .orElse(Try.failure(new Exception("Could not contact any of the branches")))
+            // Convert the resulting JSON into a map
+            .mapTry(j -> new ObjectMapper().readValue(j, Map.class))
+            // files are contained in the tree array
+            .mapTry(d -> (List<Map<Object, Object>>) d.get("tree"))
+            // each member of the tree array is an object containing a path
+            .mapTry(t -> t
+                .stream()
+                .map(u -> u.get("path").toString())
+                .filter(p -> ANT_PATH_MATCHER.matches(path, p))
+                .collect(Collectors.toList()));
   }
 
   @Override
