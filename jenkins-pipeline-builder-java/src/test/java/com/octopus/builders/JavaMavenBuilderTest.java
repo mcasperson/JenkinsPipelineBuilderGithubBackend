210c262b89a553453fa6760024ebdbda2dbbb8d0
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-07 18:28:07 +1000

    Fixed comments

--- jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
+++ jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
@@ -71,8 +71,8 @@ public class JavaMavenBuilderTest {
               // install dotnet
               .run("wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb")
               .run("dpkg -i packages-microsoft-prod.deb")
-              // install octocli
               .run("apt-get update; apt-get install -y apt-transport-https && apt-get update && apt-get install -y dotnet-sdk-5.0 dotnet-sdk-3.1")
+              // install octocli
               .run("apt update && sudo apt install -y --no-install-recommends gnupg curl ca-certificates apt-transport-https && "
                   + "curl -sSfL https://apt.octopus.com/public.key | apt-key add - && "
                   + "sh -c \"echo deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list\" && "
