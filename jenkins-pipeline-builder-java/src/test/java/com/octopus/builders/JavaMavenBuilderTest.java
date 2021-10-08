6c325e058816e23d95882392b7d5ecc898196812
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-09 06:17:03 +1000

    Trying to fix container build

--- jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
+++ jenkins-pipeline-builder-java/src/test/java/com/octopus/builders/JavaMavenBuilderTest.java
@@ -81,7 +81,7 @@ public class JavaMavenBuilderTest {
                   + "mstest:1.0.0")
               .run("apt-get update")
               // Install php, ruby, python
-              .run("apt-get install vim maven wget curl sudo python3 python3-pip python3-html5lib ruby-full ruby-dev php7.4 php-cli php-zip php-dom php-mbstring unzip -y")
+              .run("apt-get install maven wget curl sudo python3 python3-pip ruby-full ruby-dev php7.4 php-cli php-zip php-dom php-mbstring unzip -y")
               // install gradle
               .run("wget https://services.gradle.org/distributions/gradle-7.2-bin.zip")
               .run("unzip gradle-7.2-bin.zip")
