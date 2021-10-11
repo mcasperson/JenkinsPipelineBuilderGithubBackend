17ccf05d938f9d8ee04da65d7fa58800967f7a8d
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-11 11:01:52 +1000

    Captured dependency updates for dotnet projects

--- jenkins-pipeline-builder-java/src/main/java/com/octopus/builders/dotnet/DotnetCoreBuilder.java
+++ jenkins-pipeline-builder-java/src/main/java/com/octopus/builders/dotnet/DotnetCoreBuilder.java
@@ -118,6 +118,25 @@ public class DotnetCoreBuilder implements PipelineBuilder {
                     .add(new Argument("fingerprint", "true", ArgType.BOOLEAN))
                     .build())
                 .build())
+            .add(Comment.builder()
+                .content(
+                    "List any dependency updates.")
+                .build())
+            .add(FunctionManyArgs.builder()
+                .name("sh")
+                .args(new ImmutableList.Builder<Argument>()
+                    .add(new Argument("script",
+                        "dotnet list package --outdated > dependencieupdates.txt",
+                        ArgType.STRING))
+                    .build())
+                .build())
+            .add(FunctionManyArgs.builder()
+                .name("archiveArtifacts")
+                .args(new ImmutableList.Builder<Argument>()
+                    .add(new Argument("artifacts", "dependencieupdates.txt", ArgType.STRING))
+                    .add(new Argument("fingerprint", "true", ArgType.BOOLEAN))
+                    .build())
+                .build())
             .build()))
         .build();
   }
