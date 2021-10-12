0db55ff82bf5e9b5329c44f04789ffd860f19294
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-13 09:09:06 +1000

    Casting is unnecessary

--- jenkins-pipeline-builder-java/src/test/resources/jenkins/octopus_tool.groovy
+++ jenkins-pipeline-builder-java/src/test/resources/jenkins/octopus_tool.groovy
@@ -5,7 +5,7 @@ import hudson.model.*;
 import hudson.tools.*
 
 def inst = Jenkins.getInstance()
-def desc = (hudson.plugins.octopusdeploy.OctoInstallation.DescriptorImpl) inst.getDescriptorByType(hudson.plugins.octopusdeploy.OctoInstallation.DescriptorImpl)
+def desc = inst.getDescriptorByType(hudson.plugins.octopusdeploy.OctoInstallation.DescriptorImpl)
 def octoInstall = new hudson.plugins.octopusdeploy.OctoInstallation("LinuxOcto", "/usr/bin/octo")
 desc.setInstallations(octoInstall)
 desc.save()
