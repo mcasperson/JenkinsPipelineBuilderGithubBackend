109f61fcb3836656118e41a4d8a75931539f3e5d
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-13 13:18:57 +1000

    Fixed parameters

--- jenkins-pipeline-builder-java/src/main/java/com/octopus/builders/GitBuilder.java
+++ jenkins-pipeline-builder-java/src/main/java/com/octopus/builders/GitBuilder.java
@@ -183,7 +183,7 @@ public class GitBuilder {
                 .args(new ImmutableList.Builder<Argument>()
                     .add(new Argument(
                         "defaultValue",
-                        "'" + accessor.getRepoName().getOrElse("application") + "'",
+                        accessor.getRepoName().getOrElse("application"),
                         ArgType.STRING))
                     .add(new Argument(
                         "description",
